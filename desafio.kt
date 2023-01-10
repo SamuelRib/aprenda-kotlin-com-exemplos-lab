f8bb035f1d50e7bc4b431dd129431effb6a5525a
SamuelRib <121571031+SamuelRib@users.noreply.github.com>
2023-01-09 23:31:27 -0300

    Update desafio.kt
    Adicionado exemplos.

--- desafio.kt
+++ desafio.kt
@@ -2,20 +2,62 @@
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+    class Usuario(val nome: String, val email: String, val celular: Long)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+    data class ConteudoEducacional(var nome: String, val duracao: Int = 60, val basico: Nivel)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
-    val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>, val nivel: Nivel) {
+        var matriculados = mutableListOf<Usuario>()
+        fun matricular(usuario: Usuario) {
+            matriculados.add(usuario)
+        }
+
+        fun listaMatriculados(){
+            for (pessoasMatriculadas in matriculados){
+                println(pessoasMatriculadas.nome)
+            }
+        }
+
+        fun listaConteudos(){
+            for (conteudosEspecificos in conteudos )
+                println(conteudosEspecificos.nome)
+        }
     }
-}
 
-fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
-}
+    fun main() {
+        //usuarios
+        val carlos = Usuario("Carlos Henrique", "carlos@hotmail.com", 36941878912)
+        val roberta = Usuario("Roberta Nacimento", "roberta@yahoo.com", 41965478313)
+
+        //Blocos de estudo
+        val bloco_1 = ConteudoEducacional("Principio de Agilidade e Desenvolvimento de Software",17, Nivel.BASICO)
+        val bloco_2 = ConteudoEducacional ("Dominando a Linguagem de Programação Kotlin", 10, Nivel.BASICO)
+        val bloco_3 = ConteudoEducacional("Refinando Sua Técnica com Desafios de Código em Kotlin", 5, Nivel.INTERMEDIARIO )
+        val bloco_4 = ConteudoEducacional("Fortalecendo Seu Perfil Profissional", 5, Nivel.BASICO)
+        val bloco_5 = ConteudoEducacional("Criando uma Aplicação de Vendas", 20, Nivel.DIFICIL)
+
+
+        val blocosKotlinEstudo = mutableListOf<ConteudoEducacional>()
+        blocosKotlinEstudo.add(bloco_1)
+        blocosKotlinEstudo.add(bloco_2)
+        blocosKotlinEstudo.add(bloco_3)
+        blocosKotlinEstudo.add(bloco_4)
+        blocosKotlinEstudo.add(bloco_5)
+
+
+        val kotlin_experience = Formacao("Kotlin Experience", blocosKotlinEstudo,Nivel.INTERMEDIARIO)
+
+
+        kotlin_experience.matricular(carlos)
+        kotlin_experience.matricular(roberta)
+
+        println()
+        println("Curso: ${kotlin_experience.nome}")
+        println("Matriculados no Curso: ")
+        println("${kotlin_experience.listaMatriculados()}")
+        println("Blocos de estudo: ")
+        println("${kotlin_experience.listaConteudos()}")
+
+    }
+
